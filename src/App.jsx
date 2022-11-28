b2805c6363d1753a105eea305acd9a7c3cc301a6
monsef1234 <monsef4566@gmail.com>
2022-11-28 17:54:32 +0100

    optimize website

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,16 @@ const App = () => {
       <Title />
       <Groups />
       <Matches />
+      <h3 style={{ textAlign: "center", margin: "1rem" }}>
+        Code Source :
+        <a
+          href="https://github.com/monsef1234/worldCup22"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Here
+        </a>
+      </h3>
     </div>
   );
 };
